0140c6489201540157683231f50d0b9caa2a2c62
Yathartha19 <aarushyathartha19@gmail.com>
2025-10-30 22:54:27 +0530

    updated contributor link

--- app/page.tsx
+++ app/page.tsx
@@ -115,7 +115,7 @@ export default function CoursePage() {
               ))}
             </div>
 
-            <div className="hover:cursor-pointer" onClick={() => window.open("https://github.com/sumit-s-nair/mern-docs", "_blank")}>
+            <div className="hover:cursor-pointer" onClick={() => window.open("https://github.com/sumit-s-nair/mern-docs/blob/main/contributions.md", "_blank")}>
               <Card
                 className="group relative overflow-hidden border border-border/60 bg-card/60 
                           transition-all duration-300 hover:-translate-y-1 hover:shadow-lg 
